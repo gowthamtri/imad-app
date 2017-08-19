f8008a65d18ed46e6671adc635e6ef01665787b0
gowthamtri <gowthamtri@gmail.com>
2017-08-19 09:37:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one will be served here.')
+    res.send('Article one will be served here.');
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one will be served here.')
+    res.send('Article one will be served here.');
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one will be served here.')
+    res.send('Article one will be served here.');
 });
 
 app.get('/ui/style.css', function (req, res) {
