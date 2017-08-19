f417bcd4f632582ac0696929a7a9974f5b6df82f
gowthamtri <gowthamtri@gmail.com>
2017-08-19 11:30:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ element.InnerHtml= "updated content";
 
 var img = document.getElementById('img');
 img.onclick = function(){
-    img.style.marginleft = '80px';
+    img.style.marginleft = '180px';
 };
