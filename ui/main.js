2ecf60701c95e57564930981881aaf0d1dc2c32d
gowthamtri <gowthamtri@gmail.com>
2017-08-19 11:47:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ var counterCount = 0;
 var counter = document.getElementById('counter');
 var span = document.getElementById('count');
 counter.onclick = function(){
-    
+    counter = counter + 1;
     span.InnerHtml = counterCount.toString();
 }
