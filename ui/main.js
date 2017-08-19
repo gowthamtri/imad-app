a8fc5a0649ec1ba2390341bff57104a0e164d351
gowthamtri <gowthamtri@gmail.com>
2017-08-19 11:29:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,9 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text')
-element.InnerHtml= "updated content"
+var element = document.getElementById('main-text');
+element.InnerHtml= "updated content";
+
+var img = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginleft = '80px';
+};
