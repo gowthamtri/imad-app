952ca734cbb300694fed3f1710bf36a4d05b8129
gowthamtri <gowthamtri@gmail.com>
2017-08-19 11:46:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,25 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-element.InnerHtml= "updated content";
+// var element = document.getElementById('main-text');
+// element.InnerHtml= "updated content";
 
-var img = document.getElementById('img');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
+// var img = document.getElementById('img');
+// var marginLeft = 0;
+// function moveRight(){
+//     marginLeft = marginLeft + 1;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
 
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+// img.onclick = function(){
+//     var interval = setInterval(moveRight,50);
     
-};
+// };
+
+var counterCount = 0;
+
+var counter = document.getElementById('counter');
+var span = document.getElementById('count');
+counter.onclick = function(){
+    
+    span.InnerHtml = counterCount.toString();
+}
