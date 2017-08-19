b1193d5c34af824232f5ced790baf4141c588c29
gowthamtri <gowthamtri@gmail.com>
2017-08-19 11:58:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,12 +15,22 @@ console.log('Loaded!');
     
 // };
 
-var counterCount = 0;
-
 var button = document.getElementById('counter');
 
 button.onclick = function(){
-    counterCount = counterCount + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counterCount.toString();
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counterCount = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counterCount;
+            }
+        }
+    };
+    
+    request.open('GET', 'http://gowthamtri.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
