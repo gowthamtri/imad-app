223beb427f0e29fc14cd2deb577920c32f80e0b1
gowthamtri <gowthamtri@gmail.com>
2017-08-19 11:48:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,19 @@
 console.log('Loaded!');
 
+// var element = document.getElementById('main-text');
+// element.InnerHtml= "updated content";
 
+// var img = document.getElementById('img');
+// var marginLeft = 0;
+// function moveRight(){
+//     marginLeft = marginLeft + 1;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
+
+// img.onclick = function(){
+//     var interval = setInterval(moveRight,50);
+    
+// };
 
 var counterCount = 0;
 
@@ -9,4 +22,4 @@ var span = document.getElementById('count');
 counter.onclick = function(){
     counterCount = counterCount + 1;
     span.InnerHtml = counterCount.toString();
-}
+};
